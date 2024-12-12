7fa9e3bd4f47e9ef07eceac166e585f0ad27fc90
HogskinKitty <zymdev@126.com>
2024-12-13 01:53:30 +0800

    feat：更新侧边栏定义方式，增加 AI 编程相关知识

--- docs/.vitepress/navbar.ts
+++ docs/.vitepress/navbar.ts
@@ -3,4 +3,5 @@ import type {DefaultTheme} from 'vitepress'
 export const nav: DefaultTheme.Config['nav'] = [
   {text: '首页', link: '/'},
   {text: 'Java', link: '/java/basic'},
+  {text: 'AI', link: '/ai/cline-gpt'},
 ]
--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -1,25 +1,39 @@
 import type {DefaultTheme} from 'vitepress'
 
-export const sidebar: DefaultTheme.Config['sidebar'] = [
-  {
-    base: '/java/',
-    text: 'Java 基础',
-    items: [
-      {
-        base: '/java/',
-        text: '示例内容',
-        link: 'basic'
-      },
-    ],
-  },
-  {
-    base: '/java/',
-    text: 'JVM',
-    items: [
-      {
-        text: '示例内容',
-        link: 'jvm'
-      },
-    ],
-  }
-]
+export const sidebar: DefaultTheme.Config['sidebar'] = {
+  // 定义基础路径
+  '/java/': {base: '/java/', items: sidebarJava()},
+  '/ai/': {base: '/ai/', items: sidebarAi()},
+}
+
+// Java
+function sidebarJava(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: 'Java 基础',
+      items: [
+        {text: '示例内容', link: 'basic'},
+      ],
+    },
+    {
+      text: 'JVM',
+      items: [
+        {text: '示例内容', link: 'jvm'},
+      ],
+    },
+  ]
+}
+
+// AI
+function sidebarAi(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: 'AI 编程',
+      items: [
+        {text: 'Cline + GPT-4o API + LiteLLM 实现 Cursor 免费平替', link: 'cline-gpt'},
+      ],
+    },
+  ]
+}
+
+
